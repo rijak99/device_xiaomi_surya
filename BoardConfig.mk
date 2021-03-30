f80882f5ea6d2e575784d0f3b386154e7818dab0
rijak999 <rijak999@rizakkamal999@gmail.com>
2021-03-30 06:54:37 +0000

    surya: Compiled using proton clang

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -91,7 +91,7 @@ BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 TARGET_KERNEL_CONFIG := surya_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/surya
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_USE_LATEST_CLANG := true
+TARGET_KERNEL_CLANG_VERSION := 13
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 262144
